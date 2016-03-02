00758dd5c420d0470dffc68ce84671397d9a8bfe
Mario Andrés Correa <a@macool.me>
2016-03-02 08:10:26 -0500

    annotate models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id         :integer          not null, primary key
+#  titulo     :string
+#  imagen     :string
+#  precio     :float
+#  detalle    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Item < ActiveRecord::Base
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id         :integer          not null, primary key
+#  titulo     :string
+#  imagen     :string
+#  precio     :float
+#  detalle    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryGirl.define do
   factory :item do
     name "MyString"
