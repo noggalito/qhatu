9350b24cb19614819d307776d8a03e44d4570cfd
Mario Andrés Correa <a@macool.me>
2016-03-02 08:02:32 -0500

    use error flash type for errors
    and use short-notation

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,4 @@
 class ItemsController < ApplicationController
-
   def new
     @item = Item.new
   end
@@ -7,11 +6,11 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(items_params)
     if @item.save
-      redirect_to root_path
-      flash[:notice] = 'Success'
+      redirect_to root_path,
+                  notice: 'Success'
     else
-      redirect_to new_item_path
-      flash[:notice] = 'Failed'
+      redirect_to new_item_path,
+                  error: 'Something went wrong'
     end
   end
 
@@ -20,5 +19,4 @@ class ItemsController < ApplicationController
   def items_params
     params.require(:item).permit :titulo, :imagen, :precio, :detalle
   end
-
 end
