ecbac7d454119552cb29b8ecdb3b66c327fbdc25
Mario Andrés Correa <a@macool.me>
2016-03-03 07:07:38 -0500

    refactor item specs
    so that they use 'subject' helper

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -14,16 +14,18 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  subject { item }
+
   describe "factory" do
     let(:item) { build :item }
 
     it "Can be instantiated" do
-      expect(item).to be_an_instance_of(Item)
+      is_expected.to be_an_instance_of(Item)
     end
 
     it "Can be saved successfully" do
       item.save
-      expect(item).to be_persisted
+      is_expected.to be_persisted
     end
   end
 
@@ -34,22 +36,22 @@ RSpec.describe Item, type: :model do
 
     context "not a number" do
       let(:precio) { "some value" }
-      it { expect(item).to_not be_valid }
+      it { is_expected.to_not be_valid }
     end
 
     context "precio 0" do
       let(:precio) { 0 }
-      it { expect(item).to_not be_valid }
+      it { is_expected.to_not be_valid }
     end
 
     context "more than 0" do
       let(:precio) { 2.99 }
-      it { expect(item).to be_valid }
+      it { is_expected.to be_valid }
     end
 
     context "price inside string" do
       let(:precio) { "2.99" }
-      it { expect(item).to be_valid }
+      it { is_expected.to be_valid }
     end
   end
 end
