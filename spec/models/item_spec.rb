bced9fdb6254b15c72180936a3ac5561e9cad567
Mario Andrés Correa <a@macool.me>
2016-03-03 06:56:37 -0500

    validate item's price numericality

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -26,4 +26,30 @@ RSpec.describe Item, type: :model do
       expect(item).to be_persisted
     end
   end
+
+  describe ":precio" do
+    let(:item) {
+      build :item, precio: precio
+    }
+
+    context "not a number" do
+      let(:precio) { "some value" }
+      it { expect(item).to_not be_valid }
+    end
+
+    context "precio 0" do
+      let(:precio) { 0 }
+      it { expect(item).to_not be_valid }
+    end
+
+    context "more than 0" do
+      let(:precio) { 2.99 }
+      it { expect(item).to be_valid }
+    end
+
+    context "price inside string" do
+      let(:precio) { "2.99" }
+      it { expect(item).to be_valid }
+    end
+  end
 end
