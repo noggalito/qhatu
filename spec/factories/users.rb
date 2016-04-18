66b09b5f1dd891925fef9c55a267d63c7eac3956
JhymerMartinez <jmartinez@ioet.com>
2016-04-18 15:14:32 -0500

    Update specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -27,6 +27,7 @@
 
 FactoryGirl.define do
   factory :user do
+    sequence(:first_name) { |n| "User #{n}" }
     sequence(:email) { |n| "user-#{n}@qhatu.org" }
     sequence(:password) { |n| "user-password-#{n}" }
     password_confirmation { password }
